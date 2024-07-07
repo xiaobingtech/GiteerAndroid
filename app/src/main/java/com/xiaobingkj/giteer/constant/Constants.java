bbc5c336a8336f7b448a2bfc2529601c2a8ef62d
xiaobingtech <161550081+xiaobingtech@users.noreply.github.com>
2024-07-07 19:30:22 +0800

    Create Constants.java

---
+++ app/src/main/java/com/xiaobingkj/giteer/constant/Constants.java
@@ -0,0 +1,7 @@
+package com.xiaobingkj.giteer.constant;
+
+public class Constants {
+    public static final String client_id = "0b6b0e8abd56956a17a197ef2913064eea92208fc4752b224b87a9aba5470513";
+    public static final String client_secret = "aaff83a79e3bc05bf6dae8c2acd3679b9709250f9977251fd0dc79198b64f168";
+    public static final String redirect_uri = "giteer://oauth-callback";
+}
