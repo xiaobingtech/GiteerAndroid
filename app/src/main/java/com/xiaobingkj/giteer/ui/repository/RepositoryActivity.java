bbcb000077cc3db916a02af372304a4de490e2fa
xiaobingtech <161550081+xiaobingtech@users.noreply.github.com>
2024-07-14 18:20:13 +0800

    readme

--- app/src/main/java/com/xiaobingkj/giteer/ui/repository/RepositoryActivity.java
+++ app/src/main/java/com/xiaobingkj/giteer/ui/repository/RepositoryActivity.java
@@ -23,6 +23,9 @@ import org.commonmark.node.Node;
 
 import cn.carbs.android.avatarimageview.library.AvatarImageView;
 import io.noties.markwon.Markwon;
+import io.noties.markwon.ext.tables.TablePlugin;
+import io.noties.markwon.image.glide.GlideImagesPlugin;
+import io.noties.markwon.syntax.SyntaxHighlight;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import rxhttp.RxHttp;
 
@@ -50,20 +53,32 @@ public class RepositoryActivity extends AppCompatActivity {
             if (entry == null) {
                 TrendSubEntry subEntry = getIntent().getSerializableExtra("v3model", TrendSubEntry.class);
                 name = subEntry.getName_with_namespace();
+                ref = subEntry.getDefault_branch();
+                AvatarImageView avatar = binding.avatar;
+                if (subEntry.getOwner().getPortrait_url().contains("no_portrait.png")) {
+                    avatar.setTextAndColor(subEntry.getOwner().getName().substring(0, 1), R.color.gray);
+                }else{
+                    Glide.with(this).load(subEntry.getOwner().getPortrait_url()).into(avatar);
+                }
+                binding.name.setText(name);
+                binding.desc.setText(subEntry.getDescription());
+                binding.time.setText(subEntry.getLast_push_at());
+                refreshReadMe(subEntry.getPath_with_namespace());
             }else{
                 name = entry.getHuman_name();
+                ref = entry.getDefault_branch();
+                AvatarImageView avatar = binding.avatar;
+                if (entry.getOwner().getAvatar_url().equals("https://gitee.com/assets/no_portrait.png")) {
+                    avatar.setTextAndColor(entry.getOwner().getName().substring(0, 1), R.color.gray);
+                }else{
+                    Glide.with(this).load(entry.getOwner().getAvatar_url()).into(avatar);
+                }
+                binding.name.setText(name);
+                binding.desc.setText(entry.getDescription());
+                binding.time.setText(entry.getUpdated_at());
+                refreshReadMe(entry.getHuman_name());
             }
-            ref = entry.getDefault_branch();
-            AvatarImageView avatar = binding.avatar;
-            if (entry.getOwner().getAvatar_url().equals("https://gitee.com/assets/no_portrait.png")) {
-                avatar.setTextAndColor(entry.getOwner().getName().substring(0, 1), R.color.gray);
-            }else{
-                Glide.with(this).load(entry.getOwner().getAvatar_url()).into(avatar);
-            }
-            binding.name.setText(name);
-            binding.desc.setText(entry.getDescription());
-            binding.time.setText(entry.getUpdated_at());
-            refreshReadMe(entry);
+
 
 
         }else{
@@ -78,17 +93,32 @@ public class RepositoryActivity extends AppCompatActivity {
         }, this, name);
     }
 
-    void refreshReadMe(StarEntry entry) {
-        RxHttp.get("api/v5/repos/"+entry.getFull_name()+"/readme")
+    void refreshReadMe(String name) {
+        RxHttp.get("api/v5/repos/"+name+"/readme")
                 .add("ref", ref)
                 .toObservable(ReadMeEntry.class)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe( s -> {
 
-                    final Markwon markwon = Markwon.create(this);
+                    final Markwon markwon = Markwon.builder(this)
+                            .usePlugin(GlideImagesPlugin.create(this))
+                            .usePlugin(TablePlugin.create(this))
+                            .build();
+
+                    String content = new String(EncodeUtils.base64Decode(s.getContent()));
+                    // 假设这是你的基础URL
+                    String baseUrl = "https://gitee.com/"+name+"/raw/"+ref+"/";
+
+
+                    // 正则表达式匹配Markdown中的图片链接
+                    // 假设所有的相对路径都不以"http"或"https"开头
+                    String regex = "\\]\\((?!http)(.*?)\\)";
+
+                    // 替换相对路径为绝对路径
+                    String replacedContent = content.replaceAll(regex, "](<" + baseUrl + "$1>)");
 
                     // parse markdown to commonmark-java Node
-                    final Node node = markwon.parse(new String(EncodeUtils.base64Decode(s.getContent())));
+                    final Node node = markwon.parse(replacedContent);
 
                     // create styled text from parsed Node
                     final Spanned markdown = markwon.render(node);
